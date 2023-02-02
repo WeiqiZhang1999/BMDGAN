2cd3d2328e60fb369284bf6cd10e86fdef938bf0
WeiqiZhang1999 <weiqi_1999@outlook.com>
2023-02-02 15:30:29 +0900

    fix stage-two InferenceDataset.py

--- Dataset/BMDGAN/Stage2/InferenceDataset.py
+++ Dataset/BMDGAN/Stage2/InferenceDataset.py
@@ -14,6 +14,17 @@ from Utils.ImageHelper import ImageHelper
 from MultiProcessingHelper import MultiProcessingHelper
 from Utils.MetaImageHelper2 import MetaImageHelper
 import json
+from typing import AnyStr
+from dataclasses import dataclass
+
+
+@dataclass
+class MetaImageDAO:
+    case_name: str
+    slice_id: str | int
+    image_path: AnyStr
+    image_data: None | np.ndarray = None
+    spacing: None | np.ndarray = None
 
 
 class InferenceDataset(Dataset):
@@ -50,8 +61,11 @@ class InferenceDataset(Dataset):
             for slice_entry in OSHelper.scan_dirs_for_file(case_xp_dir, name_re_pattern=".+\\.mhd$"):
                 drr_path = OSHelper.path_join(self.drr_root, case_name, slice_entry.name)
                 assert OSHelper.path_exists(drr_path), drr_path
-                self.xp_pool.append(slice_entry.path)
-                self.drr_pool.append(drr_path)
+                slice_id = ''.join(slice_entry.name.split('.')[:-1])
+                xp_dao = MetaImageDAO(case_name, slice_id, image_path=slice_entry.path)
+                drr_dao = MetaImageDAO(case_name, slice_id, image_path=drr_path)
+                self.xp_pool.append(xp_dao)
+                self.drr_pool.append(drr_dao)
                 self.bmd_pool.append(self.bmd_df.loc[case_name, 'DXABMD'])
         assert len(self.xp_pool) > 0 and len(self.drr_pool) > 0 and len(self.bmd_pool) > 0
 
@@ -62,17 +76,24 @@ class InferenceDataset(Dataset):
 
         if self.preload:
             args = []
-            for xp_path in self.xp_pool:
-                args.append((xp_path, self.image_size))
-            self.xp_pool = MultiProcessingHelper().run(args=args, func=self._load_image, n_workers=self.n_worker,
+            for xp_dao in self.xp_pool:
+                args.append((xp_dao.image_path, self.image_size))
+            xps = MultiProcessingHelper().run(args=args, func=self._load_image, n_workers=self.n_worker,
                                                        desc="Loading Xp" if self.verbose else None,
                                                        mininterval=60, maxinterval=180)
+            for xp_dao, (xp, spacing) in zip(self.xp_pool, xps):
+                xp_dao.image_data = xp
+                xp_dao.spacing = spacing
+
             args = []
-            for drr_path in self.drr_pool:
-                args.append((drr_path, self.image_size))
-            self.drr_pool = MultiProcessingHelper().run(args=args, func=self._load_image, n_workers=self.n_worker,
+            for drr_dao in self.drr_pool:
+                args.append((drr_dao.image_path, self.image_size))
+            drrs = MultiProcessingHelper().run(args=args, func=self._load_image, n_workers=self.n_worker,
                                                         desc="Loading DRR" if self.verbose else None,
                                                         mininterval=60, maxinterval=180)
+            for drr_dao, (drr, spacing) in zip(self.drr_pool, drrs):
+                drr_dao.image_data = drr
+                drr_dao.spacing = spacing
 
     def __len__(self):
         return len(self.xp_pool)
@@ -81,16 +102,23 @@ class InferenceDataset(Dataset):
         return self.__getitem__(idx)
 
     def __getitem__(self, idx):
-        xp_path, drr_path = self.xp_pool[idx], self.drr_pool[idx]
+
         dxa_bmd = torch.tensor(self.bmd_pool[idx], dtype=torch.float32)
+        xp_dao, drr_dao = self.xp_pool[idx], self.drr_pool[idx]
 
         if self.preload:
-            xp, drr = xp_path.copy(), drr_path.copy()
+            xp, drr = xp_dao.image_data.copy(), drr_dao.image_data.copy()
+            spacing = xp_dao.spacing.copy()
         else:
-            xp = self._load_image(xp_path, self.image_size)
-            drr = self._load_image(drr_path, self.image_size)
+            xp, spacing = self._load_image(xp_dao.image_path, self.image_size)
+            drr, _ = self._load_image(drr_dao.image_path, self.image_size)
+        case_name = xp_dao.case_name
+        slice_id = xp_dao.slice_id
 
-        return {"xp": xp, "drr": drr, "DXABMD": dxa_bmd}
+        return {"xp": xp, "drr": drr, "spacing": spacing,
+                "case_name": case_name, "slice_id": slice_id,
+                "DXABMD": dxa_bmd}
+        # return {"xp": xp, "drr": drr, "DXABMD": dxa_bmd}
 
     @staticmethod
     def _load_image(load_path, load_size):

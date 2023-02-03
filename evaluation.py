02b2ab8923e1b7f1ddc42209cb2e43273324171b
WeiqiZhang1999 <weiqi_1999@outlook.com>
2023-02-03 17:08:00 +0900

    fix

--- evaluation.py
+++ evaluation.py
@@ -88,7 +88,7 @@ def task(case_name, fold):
     MAX_VAL_DXA_DRR_315 = 40398.234376
     THRESHOLD_DXA_BMD_315 = 1591.5
     gt_path = r'/win/salmon/user/zhangwq/deeplearning/bmd/pix2pix/dataset/DXA_DRR_315'
-    fake_path_pre = r'/win/salmon/user/zhangwq/BMD_projects/workspace/20230201_test/inference_e150/output'
+    fake_path_pre = r'/win/salmon/user/zhangwq/BMD_projects/workspace/20230201_test/inference_e310/output'
     bmd_path = r'/win/salmon/user/zhangwq/deeplearning/bmd/pix2pix/data/case_info(newCTBMD).xlsx'
     bmd_df = pd.read_excel(bmd_path, index_col=1)
     fake_path = os.path.join(fake_path_pre, fold, 'fake_drr')
@@ -169,7 +169,6 @@ def main():
 
     # fake_bmd = np.concatenate(fake_bmd_list)
     # gt_bmd = np.concatenate(gt_bmd_List)
-    print(fake_bmd_list)
     pcc = pearsonr(fake_bmd_list, gt_bmd_List)[0]
 
     print(f'Mean PSNR: %.3f' % (psnr / total_count))

4c453f4d6461d0ecf9da6e4a717603f122813619
WeiqiZhang1999 <weiqi_1999@outlook.com>
2023-02-10 00:28:04 +0900

    update

--- model/CycleBMDModel.py
+++ model/CycleBMDModel.py
@@ -60,12 +60,12 @@ class CycleBMDModel(TrainingModelInt):
 
         # Prepare models
         if self.lumbar_data and self.binary:
-            self.netG = ImportHelper.get_class(netG_up_config["class"])
-            netG_up_config.pop("class")
+            # self.netG = ImportHelper.get_class(netG_up_config["class"])
+            # netG_up_config.pop("class")
             self.netG = BaseBMDGAN(**netG_up_config).to(self.device)
 
-            self.netG_helper = ImportHelper.get_class(netG_helper_up_config["class"])
-            netG_helper_up_config.pop("class")
+            # self.netG_helper = ImportHelper.get_class(netG_helper_up_config["class"])
+            # netG_helper_up_config.pop("class")
             self.netG_helper = BaseBinaryMaskBMDGAN(**netG_helper_up_config).to(self.device)
 
             self.netD = MultiscaleDiscriminator(input_nc=3).to(self.device)

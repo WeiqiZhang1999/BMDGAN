56c27666d6b88bb14865d450ec99557a16f2a395
WeiqiZhang1999 <weiqi_1999@outlook.com>
2023-02-10 15:52:08 +0900

    update

--- evaluation_lumbar.py
+++ evaluation_lumbar.py
@@ -143,7 +143,7 @@ def task2(case_name, fold):
     MAX_VAL_DXA_MASK_DRR_315 = 91.80859
     # THRESHOLD_DXA_BMD_315_list = np.linspace(1, 1500, 100, dtype=np.float64)
     gt_path = r'/win/salmon/user/zhangwq/data/20230128_Lumbar_DRRs_perspective_uncalibrated_AP_ensembles'
-    fake_path_pre = r'/win/salmon/user/zhangwq/BMD_projects/workspace/lumbar_test/inference_direct_new_mask_e1270/output'
+    fake_path_pre = r'/win/salmon/user/zhangwq/BMD_projects/workspace/lumbar_test/inference_direct_new_mask_e630/output'
     bmd_path = r'/win/salmon/user/zhangwq/data/Spine_data_for_AI_celan_20230119.xlsx'
     bmd_df = pd.read_excel(bmd_path, index_col=0)
     bmd_df.rename({'Unnamed: 77': 'DXABMD'}, axis=1, inplace=True)
@@ -197,7 +197,7 @@ def main():
     fold_list = os.listdir(fake_path)
 
     args = []
-    THRESHOLD_DXA_BMD_315 = 2.45e-5
+    THRESHOLD_DXA_BMD_315 = 2.4e-5
     # THRESHOLD_DXA_BMD_315_list = np.linspace(0, 10, 1000, dtype=np.float32)
     # THRESHOLD_DXA_BMD_315_list = [0.1, 0.5, 1.0]
 

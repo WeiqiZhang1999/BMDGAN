30babb8e2b077d731714652d97a93af57e8649cc
WeiqiZhang1999 <weiqi_1999@outlook.com>
2023-02-22 14:34:53 +0900

    update

--- Experiment/TrainingModel2.py
+++ Experiment/TrainingModel2.py
@@ -74,7 +74,7 @@ class TrainingModel(BaseExperiment):
 
         if self.__model_name.split('_')[-1] == '2':
             self._tb_path = OSHelper.path_join(
-                OSHelper.format_path(r"/win/salmon\user\zhangwq\BMD_projects\workspace\finetune2"),
+                OSHelper.format_path(r"/win/salmon\user\zhangwq\BMD_projects\workspace\finetune"),
                 "logs")
         else:
             self._tb_path = OSHelper.path_join(

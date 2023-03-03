e21a0245013d67d95fe7ddc0644ee90932890777
WeiqiZhang1999 <weiqi_1999@outlook.com>
2023-03-03 19:34:19 +0900

    update

--- Dataset/BMDGAN/Stage2/LumbarInferenceDataset.py
+++ Dataset/BMDGAN/Stage2/LumbarInferenceDataset.py
@@ -313,3 +313,99 @@ class LumbarBinaryMaskInferenceDataset(Dataset):
         img = np.clip(img, -1., 1.)
         return img.astype(np.float32), spacing
 
+
+class AndoInferenceDataset(Dataset):
+
+    def __init__(self,
+                 split_fold: int | str,
+                 image_size: tuple[int, int],
+                 n_worker,
+                 view='AP',
+                 preload=True,
+                 need_mask=True,
+                 verbose=False):
+        self.need_mask = need_mask
+        self.view = view
+        self.split_fold = split_fold
+        self.image_size = image_size
+        self.n_worker = n_worker
+        self.preload = preload
+        self.verbose = verbose
+
+
+        self.data_root = OSHelper.format_path(r"/win/salmon\user\zhangwq\data")
+
+        with open(OSHelper.path_join(self.data_root, r"Ando_test_188.json"), 'r') as f:
+            training_case_names = json.load(f)[str(split_fold)]["test"]
+
+        self.xp_root = OSHelper.path_join(self.data_root, f"Ando_Xp_MHD_phase3_{self.view}_padding")
+
+        self.xp_pool = []
+
+        self.bmd_pool = []
+        self.bmd_df_root = OSHelper.path_join(self.data_root, "bmd_analyze.xlsx")
+        self.bmd_df = pd.read_excel(self.bmd_df_root, index_col=0)
+        for case_name in training_case_names:
+            xp_case_name = f"{case_name}.mhd"
+            case_xp_dir = OSHelper.path_join(self.xp_root, xp_case_name)
+
+            self.bmd_pool.append(self.bmd_df.loc[case_name, 'DXA Spine'])
+
+            xp_dao = MetaImageDAO(case_name, image_path=case_xp_dir)
+            self.xp_pool.append(xp_dao)
+        assert len(self.xp_pool) > 0 and len(self.bmd_pool) > 0
+
+        if self.verbose:
+            print("Test Datasets")
+            print(f"Xp: {len(self.xp_pool)}")
+            print(f"DXABMD: {len(self.bmd_pool)}")
+
+        if self.preload:
+            args = []
+            for xp_dao in self.xp_pool:
+                args.append((xp_dao.image_path, self.image_size))
+            xps = MultiProcessingHelper().run(args=args, func=self._load_image, n_workers=self.n_worker,
+                                              desc="Loading Xp" if self.verbose else None,
+                                              mininterval=60, maxinterval=180)
+            for xp_dao, (xp, spacing) in zip(self.xp_pool, xps):
+                xp_dao.image_data = xp
+                xp_dao.spacing = spacing
+
+
+    def __len__(self):
+        return len(self.xp_pool)
+
+    def get_item(self, idx):
+        return self.__getitem__(idx)
+
+    def __getitem__(self, idx):
+        dxa_bmd = torch.tensor(self.bmd_pool[idx], dtype=torch.float32)
+        xp_dao = self.xp_pool[idx]
+        if self.preload:
+            xp = xp_dao.image_data.copy()
+        else:
+            xp = self._load_image(xp_dao.image_path, self.image_size)
+        case_name = xp_dao.case_name
+
+        return {"xp": xp, "DXABMD": dxa_bmd, "case_name": case_name}
+
+    @staticmethod
+    def _load_image(load_path, load_size):
+        img, spacing = MetaImageHelper.read(load_path)
+        if img.ndim < 3:
+            img = img[..., np.newaxis]
+            spacing = np.concatenate([spacing, np.ones((1,))])
+        else:
+            img = np.transpose(img, (1, 2, 0))  # (H, W, 1)
+            temp_spacing = spacing.copy()
+            spacing[0], spacing[1], spacing[2] = temp_spacing[1], temp_spacing[2], temp_spacing[0]
+        img = img.astype(np.float64)
+
+        # img = ImageHelper.resize(img, output_shape=load_size)
+        img, spacing = MetaImageHelper.resize_2D(img, spacing, output_shape=load_size)  # [-1, 1] (H, W, 1)
+
+        img = np.transpose(img, (2, 0, 1))  # (1, H, W)
+        temp_spacing = spacing.copy()
+        spacing[0], spacing[1], spacing[2] = temp_spacing[2], temp_spacing[0], temp_spacing[1]
+        img = np.clip(img, -1., 1.)
+        return img.astype(np.float32), spacing
